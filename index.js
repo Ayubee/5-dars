ddbc1e5e8fc164df23f0274d90dfcbd807da3c21
Ayubxon <aubsattorov@gmail.com>
2025-01-22 00:27:56 +0500

    Add commented code examples for various JavaScript loops and calculations

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
-// For1
+// // For1
+// let k = 5;
+// let n = 3;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+// // For2
+// let pricePerKg = 12.5;
+// for (let weight = 0.1; weight <= 1.0; weight += 0.1) {
+//     let cost = weight * pricePerKg;
+//     console.log(`${weight.toFixed(1)} kg konfetning narxi: ${cost.toFixed(2)} so'm`);
+// }
+
+// For3
+// let n = 5;
+// let S = 0;
+// for (let i = n; i <= 2 * n; i++) {
+//   S += i ** 2;
+// }
+// console.log(`Yig'indi S = ${S}`);
+
+// For4
+// let N = 5;
+// let S = 0;
+// for (let i = 1; i <= N; i++) {
+//     S += i ** (N - i + 1);
+// }
+// console.log(`Yig'indi S = ${S}`);
+
+// For5
+// let A = 2;
+// let B = 5;
+// for (let i = A; i <= B; i++) {
+//   for (let j = 0; j < i; j++) {
+//     console.log(i);
+//   }
+// }
+
+// For6
+// let number = 12;
+// let divisors = [];
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     divisors.push(i);
+//   }
+// }
+// console.log(`Son ${number} ning bo'luvchilari ${divisors.join(", ")}`);
+
+// While1
+// let number = 12;
+// let divisors = [];
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     divisors.push(i);
+//   }
+// }
+// console.log(`Son ${number} ning bo'luvchilari: ${divisors.join(", ")}`);
+
+// While2
+// let N = 17;
+// let K = 5;
+// let quotient = 0;
+// let remainder = N;
+
+// while (remainder >= K) {
+//   remainder -= K;
+//   quotient++;
+// }b
+
+// console.log(`Bo'linmaning butun qismi: ${quotient}`);
+// console.log(`Bo'linmaning qoldig'i: ${remainder}`);
+
+// for1
+// let a = 3;
+// let b = 7;
+
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   count++;
+// }
+
+// console.log(`Chiqilgan sonlar soni: ${count}`);
+
+// for2
+// let a = 3;
+// let b = 7;
+
+// let count = 0;
+
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+//   count++;
+// }
+
+// console.log(`Chiqilgan sonlar soni: ${count}`);
