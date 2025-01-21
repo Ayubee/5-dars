3808587a8f491466f138be5a724fa64d63ab1a9d
Ayubxon <aubsattorov@gmail.com>
2025-01-21 22:25:56 +0500

    Add initial HTML structure and JavaScript file for functionality

---
+++ app.js
@@ -0,0 +1,61 @@
+// let i = +prompt("");
+
+// for (let num = 1; num < 11; num++) {
+//   console.log(`${i} x ${num} = ${i * num}`);
+// }
+
+// let text = "m";
+
+// let counter = 0;
+
+// for (let i = 0; i < text.length; i++) {
+//   if (text[i].toLowerCase() == "m") {
+//     counter++;
+//   }
+// }
+
+// console.log(counter);
+
+// let sum = 0;
+// const n = 100;
+
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 == 1) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// let number = "154623154632015478956147895614789565164879456";
+
+// for (let i = 0; i < number.length; i++) {
+//   if (number[i] == "8") {
+//     console.log(true);
+//     break;
+//   }
+// }
+
+// // console.log(number);
+
+// let counter = 0;
+// let sum = 0;
+// for (let i = 1; i <= 12; i++) {
+//   if (12 % i == 0) {
+//     counter++;
+//     sum += i;
+//   }
+// }
+
+// let k = 5;
+// let n = 11;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// let narx = 68214;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log()
+// }
